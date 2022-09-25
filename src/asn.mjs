771307426ce8e12d59c6b36e0ba5e40beb5e750d
Wany <sung@wany.io>
2022-09-25 13:31:20 +0900

    add files

---
+++ src/asn.mjs
@@ -0,0 +1,64 @@
+/**
+ * geoip-asn
+ *
+ * asn.mjs
+ *
+ * (c) 2022 Wany
+ *
+ * @summary Get asn of ip
+ * @author Wany <sung@wany.io>
+ */
+
+import fs from 'fs';
+import IP from './ip.mjs';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+const ___filename = fileURLToPath(import.meta.url);
+const ___dirname = path.dirname(___filename);
+const ip2as = JSON.parse(
+  fs.readFileSync(path.resolve(___dirname, '../data/asn-ip-as.json'))
+);
+const as2name = JSON.parse(
+  fs.readFileSync(path.resolve(___dirname, '../data/asn-as-name.json'))
+);
+
+/**
+ * @async
+ * @function query
+ * @desc Get AS infrmation about ip address
+ * @param {string} ip
+ */
+function query(ip) {
+  let result = {
+    ip: ip,
+    number: null,
+    name: '',
+  };
+
+  if (!ip2as || !as2name) {
+    return result;
+  }
+
+  const ipa = new IP(ip);
+
+  const m = ip.match(/(\d+)\.(\d+\.\d+\.\d+)/);
+  if (!m) {
+    return result;
+  }
+  const fn = m[1] * 1;
+  const set = ip2as[fn];
+  for (const key in set) {
+    if (ipa.in(new IP(fn + '.' + key))) {
+      result.number = set[key] * 1;
+      result.name = as2name[result.number];
+      break;
+    }
+  }
+
+  return result;
+}
+
+export default query;
+
+export { query };
---
+++ src/index.mjs
@@ -0,0 +1,11 @@
+/**
+ * geoip-asn
+ *
+ * (c) 2022 Wany
+ *
+ * @summary Module Index
+ * @author Wany <sung@wany.io>
+ */
+
+import geoip from './geoip.mjs';
+export default geoip;
